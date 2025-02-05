2f10e840b58809bda5b3bf15efbc7d9246c2cb8e
AKrystabel <anastasia.krystabel@gmail.com>
2025-02-05 15:54:55 -0500

    made changes how it is written

--- Game.java
+++ Game.java
@@ -17,72 +17,29 @@ public class Game {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        // initializing the scores of user 1 and 2 to be 0, as well as the rolled_number to be 0
         int user1_score = 0;
         int user2_score = 0;
         int rolled_number = 0;
+        int die_reader = 0 ; 
         
-        // creating new scannner object
-        Scanner scanner = new Scanner(System.in);        
+        Scanner scanner = new Scanner(System.in); 
         
-        // creating die1 and die2 object
         Die die1 = new Die();
         Die die2 = new Die();
         
-        // asking if user wants to play the game, 1 for yes and 0 for no.
-        // displays the initial scores, 0, for both users
         System.out.println("Do you want to play the game? 1 (yes) or 0 (no): ");
         int input_play = scanner.nextInt();
         System.out.printf("Initial Score, User 1: %d & User 2: %d\n", user1_score, user2_score);
         
-        while(input_play != 0){
-            do{
-                System.out.println("User 1, which die would you like to use? (1 or 2): ");
-                int die_reader = scanner.nextInt();
-                if (die_reader == 1){
-                    rolled_number = die1.throw_die();
-                    user1_score = user1_score + rolled_number;
-                    System.out.printf("The value from Die 1 is: %d\n", rolled_number);
-                    System.out.printf("Added to User Account, Current Score: %d\n", user1_score);
-                    System.out.println("User 2, which die would you like to use? (1 or 2): ");
-                    die_reader = scanner.nextInt();
-                    if (die_reader == 1){
-                        rolled_number = die1.throw_die();
-                        user2_score = user2_score + rolled_number;
-                        System.out.printf("The value from Die 1 is: %d\n", rolled_number);
-                        System.out.printf("Added to User Account, Current Score: %d\n", user2_score);
-                    }else{
-                        rolled_number = die2.throw_die();
-                        user2_score = user2_score + rolled_number;
-                        System.out.printf("The value from Die 2 is: %d\n", rolled_number);
-                        System.out.printf("Added to User Account, Current Score: %d\n", user2_score);
-                    }
-                    System.out.println("Do you want to continue playing the game? 1 (yes) or 0 (no): ");
-                    input_play = scanner.nextInt();
-                }else{
-                    rolled_number = die2.throw_die();
-                    user1_score = user2_score + rolled_number;
-                    System.out.printf("The value from Die 2 is: %d\n", rolled_number);
-                    System.out.printf("Added to User Account, Current Score: %d\n", user1_score);
-                    System.out.println("User 2, which die would you like to use? (1 or 2): ");
-                    die_reader = scanner.nextInt();
-                    if (die_reader == 1){
-                        rolled_number = die1.throw_die();
-                        user2_score = user2_score + rolled_number;
-                        System.out.printf("The value from Die 1 is: %d\n", rolled_number);
-                        System.out.printf("Added to User Account, Current Score: %d\n", user2_score);
-                    }else{
-                        rolled_number = die2.throw_die();
-                        user2_score = user2_score + rolled_number;
-                        System.out.printf("The value from Die 2 is: %d\n", rolled_number);
-                        System.out.printf("Added to User Account, Current Score: %d\n", user2_score);
-                    }
-                    System.out.println("Do you want to continue playing the game? 1 (yes) or 0 (no): ");
-                    input_play = scanner.nextInt();
-                } 
-            }while (input_play == 1);
-        }
+        
+        do{
+            user1_score = user1_scoring(die1,die2,die_reader,user1_score,rolled_number,scanner);
+            user2_score = user2_scoring(die1,die2,die_reader,user2_score,rolled_number,scanner);
+            System.out.println("Do you want to continue playing the game? 1 (yes) or 0 (no): ");
+            input_play = scanner.nextInt();
+        }while(input_play !=0);
+        
+        
         
         System.out.printf("User 1 Score: %d \nUser 2 Score: %d\n", user1_score,user2_score);
         if(user1_score>user2_score){
@@ -92,8 +49,39 @@ public class Game {
         }else{
             System.out.println("The winner is User 2!");
         }
-        
+    }
+  
+    
+    public static int user1_scoring(Die d1, Die d2, int die_reader, int user1_score, int rolled_number, Scanner scanner){
+        System.out.println("User 1, which die would you like to use? (1 or 2): ");
+        die_reader = scanner.nextInt();
+        if (die_reader == 1){
+           rolled_number = d1.throw_die();
+           user1_score = user1_score+rolled_number;
+        }else{
+            rolled_number = d2.throw_die();
+            user1_score = user1_score + rolled_number;
+        }
+        System.out.println("The rolled number is: " + rolled_number);
+        System.out.println("Added to User 1 Account");
+        return user1_score;
+    
     }
     
+    public static int user2_scoring(Die d1, Die d2, int die_reader, int user2_score, int rolled_number, Scanner scanner){
+        System.out.println("User 2, which die would you like to use? (1 or 2): ");
+        die_reader = scanner.nextInt();
+        if (die_reader == 1){
+           rolled_number = d1.throw_die();
+           user2_score = user2_score+rolled_number;
+        }else{
+            rolled_number = d2.throw_die();
+            user2_score = user2_score + rolled_number;
+        }
+        System.out.println("The rolled number is: " + rolled_number);
+        System.out.println("Added to User 2 Account");
+        return user2_score;
+    
+    }
     
 }
