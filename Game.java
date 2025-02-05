59dc3ca3d6b0ac4da0623d19cf2e4efe2ee18a03
AKrystabel <anastasia.krystabel@gmail.com>
2025-02-05 16:09:23 -0500

    still fixing main

--- Game.java
+++ Game.java
@@ -15,14 +15,15 @@ public class Game {
     /**
      * @param args the command line arguments
      */
+    
+    private static Scanner scanner = new Scanner(System.in);
+    
     public static void main(String[] args) {
         // TODO code application logic here
         int user1_score = 0;
         int user2_score = 0;
         int rolled_number = 0;
         int die_reader = 0 ; 
-        
-        Scanner scanner = new Scanner(System.in); 
         
         Die die1 = new Die();
         Die die2 = new Die();
@@ -33,8 +34,12 @@ public class Game {
         
         
         do{
-            user1_score = user1_scoring(die1,die2,die_reader,user1_score,rolled_number,scanner);
-            user2_score = user2_scoring(die1,die2,die_reader,user2_score,rolled_number,scanner);
+            System.out.println("User 1, which die would you like to use? (1 or 2): ");
+            user1_score = user_scoring(die1,die2,die_reader,user1_score,rolled_number);
+            System.out.println("Added to User 1 Account");
+            System.out.println("User 2, which die would you like to use? (1 or 2): ");
+            user2_score = user_scoring(die1,die2,die_reader,user2_score,rolled_number);
+            System.out.println("Added to User 2 Account");
             System.out.println("Do you want to continue playing the game? 1 (yes) or 0 (no): ");
             input_play = scanner.nextInt();
         }while(input_play !=0);
@@ -52,35 +57,20 @@ public class Game {
     }
   
     
-    public static int user1_scoring(Die d1, Die d2, int die_reader, int user1_score, int rolled_number, Scanner scanner){
-        System.out.println("User 1, which die would you like to use? (1 or 2): ");
+    
+    
+   public static int user_scoring(Die d1, Die d2, int die_reader, int user_score, int rolled_number){
+        
         die_reader = scanner.nextInt();
         if (die_reader == 1){
            rolled_number = d1.throw_die();
-           user1_score = user1_score+rolled_number;
+           user_score = user_score+rolled_number;
         }else{
             rolled_number = d2.throw_die();
-            user1_score = user1_score + rolled_number;
+            user_score = user_score + rolled_number;
         }
         System.out.println("The rolled number is: " + rolled_number);
-        System.out.println("Added to User 1 Account");
-        return user1_score;
-    
-    }
-    
-    public static int user2_scoring(Die d1, Die d2, int die_reader, int user2_score, int rolled_number, Scanner scanner){
-        System.out.println("User 2, which die would you like to use? (1 or 2): ");
-        die_reader = scanner.nextInt();
-        if (die_reader == 1){
-           rolled_number = d1.throw_die();
-           user2_score = user2_score+rolled_number;
-        }else{
-            rolled_number = d2.throw_die();
-            user2_score = user2_score + rolled_number;
-        }
-        System.out.println("The rolled number is: " + rolled_number);
-        System.out.println("Added to User 2 Account");
-        return user2_score;
+        return user_score;
     
     }
     
